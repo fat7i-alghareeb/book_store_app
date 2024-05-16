53ef27f20e4f2d31afdf022edbf2191b98b5e8c6
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-05-16 14:42:52 +0300

    adding newest books shimmer widget

---
+++ lib/Features/home/presentation/views/widgets/shimmer effects/newest_books_shimmer.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../../../Core/shared/widgets/shimmer.dart';
+import '../../../../../../constants.dart';
+
+class NewestBooksShimmer extends StatelessWidget {
+  const NewestBooksShimmer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: size.height * .25,
+        child: ListView.builder(
+          itemCount: 3,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.bottomStart,
+                  children: [
+                    ShimmerWidget(
+                      paddingLTRB: const [20, 0, 0, 0],
+                      height: size.height * .21,
+                      width: size.width * 0.8,
+                      borderRadius: kBorderRadius,
+                    ),
+                    ShimmerWidget(
+                      paddingLTRB: const [30, 0, 0, 10],
+                      height: size.height * .23,
+                      width: (size.height * .23) * 2.6 / 4,
+                      borderRadius: kBorderRadius,
+                    )
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
