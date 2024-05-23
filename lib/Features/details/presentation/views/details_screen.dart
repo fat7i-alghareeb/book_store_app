99e11d8aadcfd9c31089e371dbbe4fdcc4d8a1bc
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-05-23 13:43:53 +0300

    start building the ui for adding books for favorite box and saved box

--- lib/Features/details/presentation/views/details_screen.dart
+++ lib/Features/details/presentation/views/details_screen.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../../Core/domain/entities/book_entity.dart';
 import '../../../../Core/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../manger/cubit/add_books_cubit.dart';
 import 'widgets/book_option_widget.dart';
 import 'widgets/main_details_widget.dart';
 
@@ -21,44 +24,64 @@ class _DetailsScreenState extends State<DetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    HapticFeedback.heavyImpact();
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back_ios_new_rounded,
-                    size: 30,
-                  ),
-                ),
-              ),
-              MainDetailsWidget(book: widget.book, height: height * 0.25),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30.0),
-                child: BookOptionWidget(hight: height * .07),
-              ),
-              const Text(
-                "About this Book : ",
-                style: Styles.textStyle24,
-              ),
-              Text(
-                widget.book.description,
-                style: Styles.textStyle14,
-              ),
-            ],
+          child: DetailsBody(
+            book: widget.book,
           ),
         ),
       ),
+    );
+  }
+}
+
+class DetailsBody extends StatelessWidget {
+  const DetailsBody({
+    super.key,
+    required this.book,
+  });
+
+  final BookEntity book;
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0),
+          child: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+              HapticFeedback.heavyImpact();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new_rounded,
+              size: 30,
+            ),
+          ),
+        ),
+        MainDetailsWidget(book: book, height: height * 0.25),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30.0),
+          child: BookOptionWidget(
+            hight: height * .07,
+            book: book,
+          ),
+        ),
+        const Text(
+          "About this Book : ",
+          style: Styles.textStyle24,
+        ),
+        Text(
+          book.description,
+          style: Styles.textStyle14,
+        ),
+      ],
     );
   }
 }
--- lib/Features/details/presentation/views/widgets/book_option_widget.dart
+++ lib/Features/details/presentation/views/widgets/book_option_widget.dart
@@ -1,13 +1,20 @@
+import 'package:book_app/Core/domain/entities/book_entity.dart';
+import 'package:book_app/Features/details/presentation/manger/cubit/add_books_cubit.dart';
+import 'package:book_app/constants.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import '../../../../../Core/utils/text_styles.dart';
 
 class BookOptionWidget extends StatelessWidget {
   const BookOptionWidget({
     super.key,
     required this.hight,
+    required this.book,
   });
   final double hight;
+  final BookEntity book;
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -20,7 +27,7 @@ class BookOptionWidget extends StatelessWidget {
             width: width * .6,
             decoration: BoxDecoration(
               color: Theme.of(context).colorScheme.secondary.withOpacity(0.9),
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(Constants.kBorderRadius),
             ),
             child: Center(
               child: Text(
@@ -35,19 +42,111 @@ class BookOptionWidget extends StatelessWidget {
             width: 10,
           ),
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondary.withOpacity(0.9),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.format_list_bulleted_add,
-                  size: 30,
-                  color: Theme.of(context).colorScheme.primary,
+            child: InkWell(
+              onTap: () {
+                HapticFeedback.heavyImpact();
+                showBottomSheet(context);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color:
+                      Theme.of(context).colorScheme.secondary.withOpacity(0.9),
+                  borderRadius: BorderRadius.circular(Constants.kBorderRadius),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.format_list_bulleted_add,
+                    size: 30,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> showBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      useSafeArea: true,
+      builder: (context) {
+        return BlocProvider(
+          create: (context) => AddBooksCubit(),
+          child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              title: InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                  HapticFeedback.heavyImpact();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image.asset(
+                    "images/expand-button.png",
+                    height: 23,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              ),
+            ),
+            body: BottomSheetBody(
+              book: book,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BottomSheetBody extends StatefulWidget {
+  const BottomSheetBody({
+    super.key,
+    required this.book,
+  });
+  final BookEntity book;
+  @override
+  State<BottomSheetBody> createState() => _BottomSheetBodyState();
+}
+
+class _BottomSheetBodyState extends State<BottomSheetBody> {
+  bool isChecked = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Save book",
+                style: Styles.textStyle24.copyWith(
+                  fontStyle: FontStyle.italic,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              Checkbox(
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.secondary,
+                  width: 2,
+                ),
+                value: isChecked,
+                onChanged: (value) {
+                  setState(() {
+                    isChecked = value!;
+                  });
+                  BlocProvider.of<AddBooksCubit>(context)
+                      .addToSaved(widget.book);
+                },
+              ),
+            ],
           ),
         ],
       ),
